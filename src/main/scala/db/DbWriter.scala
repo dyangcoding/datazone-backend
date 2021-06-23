025dcf6034507287429ac411f4cc0e3b7573059a
DY <Dong.Yang@Student.HTW-Berlin.de>
2021-06-23 21:48:41 +0200

    setup bason reader and writer

--- src/main/scala/db/DbWriter.scala
+++ src/main/scala/db/DbWriter.scala
@@ -1,11 +1,9 @@
 package db
 
-import reactivemongo.api.MongoConnection.ParsedURI
 import reactivemongo.api.bson.collection.BSONCollection
 
 import scala.concurrent.{ExecutionContext, Future}
-import reactivemongo.api.{AsyncDriver, Collection, Cursor, DB, MongoConnection}
-import reactivemongo.api.bson.{BSONDocumentReader, BSONDocumentWriter, Macros, document}
+import reactivemongo.api.{AsyncDriver, DB, MongoConnection}
 import tweets.Tweet
 
 object DbWriter {
@@ -17,22 +15,21 @@ object DbWriter {
 
   // Connect to the database: Must be done only once per application
   val driver: AsyncDriver = AsyncDriver()
-  val parsedUri: Future[ParsedURI] = MongoConnection.fromString(mongoUri)
 
   // Database and collections: Get references
-  val futureConnection: Future[MongoConnection] = parsedUri.flatMap(driver.connect)
+  val futureConnection: Future[MongoConnection] = driver.connect(mongoUri)
   def dataZone: Future[DB] = futureConnection.flatMap(_.database("dataZone"))
 
   def tweetsCollection: Future[BSONCollection] = dataZone.map(_.collection("tweets"))
 
   // Write Documents: insert
-  implicit def twitterWriter: BSONDocumentWriter[Tweet] = Macros.writer[Tweet]
+  // implicit def twitterWriter: BSONDocumentWriter[Tweet] = Macros.writer[Tweet]
   // or provide a custom one
 
   // use personWriter
   def createTweet(tweet: Tweet): Future[Unit] =
     tweetsCollection.flatMap(_.insert.one(tweet).map(_ => {}))
 
-  implicit def tweetsReader: BSONDocumentReader[Tweet] = Macros.reader[Tweet]
+  // implicit def tweetsReader: BSONDocumentReader[Tweet] = Macros.reader[Tweet]
   // or provide a custom one
 }
--- src/main/scala/tweets/Tweet.scala
+++ src/main/scala/tweets/Tweet.scala
@@ -1,22 +1,47 @@
 package tweets
 
+import reactivemongo.api.bson._
+import reactivemongo.api.bson.{BSONReader, BSONWriter, Macros}
 import org.bson.Document
+import reactivemongo.api.bson.Macros.Annotations.Reader
+import tweets.Context.contextSeqReader
+import tweets.Entities.entitiesSeqReader
 
 import java.util
+import scala.language.postfixOps
 
 case class Domain(id: String, name: String, description: String)
+case object Domain { val domain: BSONDocumentHandler[Domain] = Macros.handler[Domain] }
+
 case class Entity(id: String, name: String)
+case object Entity { val entity: BSONDocumentHandler[Entity] = Macros.handler[Entity] }
+
 case class Context(domain: Domain, entity: Entity)
+case object Context {
+  implicit val domain: BSONDocumentHandler[Domain] = Macros.handler[Domain]
+  implicit val entity: BSONDocumentHandler[Entity] = Macros.handler[Entity]
+
+  val contextHandler: BSONHandler[Context] = Macros.handler[Context]
+
+  val contextSeqReader: BSONReader[Seq[Context]] = BSONReader.iterable[Context, Seq](contextHandler readTry)
+  val contextSeqWriter: BSONWriter[Seq[Context]] = BSONWriter.sequence[Context](contextHandler writeTry)
+}
 
 // TODO Entities represents a complex twitter entity annotation
 case class Entities()
+
+case object Entities {
+  val entitiesHandler: BSONHandler[Entities] = Macros.handler[Entities]
+  val entitiesSeqReader: BSONReader[Seq[Entities]] = BSONReader.iterable[Entities, Seq](entitiesHandler readTry)
+  val entitiesSeqWriter: BSONWriter[Seq[Entities]] = BSONWriter.sequence[Entities](entitiesHandler writeTry)
+}
 
 case class Tweet(
                   id:String,
                   text: String,
                   createdDate:String,
-                  context: List[Context],
-                  entities: List[Entities],
+                  @Reader(contextSeqReader) context: Seq[Context],
+                  @Reader(entitiesSeqReader) entities: Seq[Entities],
                   conversationId: String,
                   lang: String) {
   def toDocument: Document = {
@@ -43,6 +68,12 @@ case class Tweet(
 
 case object Tweet {
   def createTweet(json: String): Option[Tweet] = {
-
+    ???
   }
+
+  implicit val domain: BSONDocumentHandler[Domain] = Macros.handler[Domain]
+  implicit val entity: BSONDocumentHandler[Entity] = Macros.handler[Entity]
+  implicit val context: BSONDocumentHandler[Context] = Macros.handler[Context]
+  implicit val entities: BSONDocumentHandler[Entities] = Macros.handler[Entities]
+  implicit val TweetHandler: BSONDocumentHandler[Tweet] = Macros.handler[Tweet]
 }
