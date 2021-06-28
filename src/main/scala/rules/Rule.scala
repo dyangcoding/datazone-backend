f2fe36a2453a55418facae8a5234191f584e9462
DY <Dong.Yang@Student.HTW-Berlin.de>
2021-06-28 20:09:57 +0200

    extract multiple rule models

--- src/main/scala/rules/Rule.scala
+++ src/main/scala/rules/Rule.scala
@@ -2,108 +2,92 @@ package rules
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
-
 import utils.StringUtils._
 
-case class Payload(operation: String, entries: Seq[RulePayload])
-
-case class RulePayload (value: String, tag: Option[String] = None) {
-  def flatMap(transformer: rules.RulePayload => rules.RulePayload): rules.RulePayload = {
-    transformer(this)
-  }
-
-  def appendHashtag(hashtag: String): RulePayload = {
-    this.flatMap(payload => RulePayload(And(hashtag, payload.value)))
-  }
-
-  def appendUserId(userId: String): RulePayload = {
-    this.flatMap(payload => RulePayload(And(AppendAt(userId), payload.value)))
-  }
-
-  def appendFromUser(fromUser: String): RulePayload = {
-    this.flatMap(payload => RulePayload(And(Append("from:", fromUser), payload.value)))
-  }
-
-  def appendToUser(toUser: String): RulePayload = {
-    this.flatMap(payload => RulePayload(And(Append("to:", toUser), payload.value)))
-  }
-
-  def appendTag(tagValue: String): RulePayload = {
-    this.flatMap(payload => RulePayload(payload.value, tag=Option(tagValue)))
-  }
-
-  def appendEmoji(emoji: String): RulePayload = {
-    this.flatMap(payload => RulePayload(And(emoji, payload.value)))
-  }
-
-  def appendUrl(url: String): RulePayload = {
-    this.flatMap(payload => RulePayload(And(url, payload.value)))
-  }
-
-  def appendPhrase(phrase: String): RulePayload = {
-    this.flatMap(payload => RulePayload(And(phrase, payload.value)))
-  }
-
-  def appendContext(context: String): RulePayload = {
-    this.flatMap(payload => RulePayload(And(Append("context:", context), payload.value)))
-  }
-
-  def appendEntity(entity: String): RulePayload = {
-    this.flatMap(payload => RulePayload(And(Append("entity:", entity), payload.value)))
-  }
-
-  def appendConversationId(conversationId: String): RulePayload = {
-    this.flatMap(payload => RulePayload(And(Append("conversationId:", conversationId), payload.value)))
-  }
-
-  def group(): RulePayload = {
-    this.flatMap(payload => RulePayload(Group(payload.value)))
-  }
+trait Rule {
+  val isRetweet:  Option[Boolean]=Option(false)
+  val isVerified: Option[Boolean]=Option(true)
+  def toPayload: PayloadEntry
 }
 
-/*
- Represents optional options for building rules for filtered Stream Endpoint
- */
-case class RuleOptions(
-     isRetweet:   Option[Boolean] = Option(false),
-     isReply:     Option[Boolean] = Option(false),
-     isVerified:  Option[Boolean] = Option(true),
-     hasHashTags: Option[Boolean] = Option(true),
-     hasLinks:    Option[Boolean] = Option(true),
-     hasMedia:    Option[Boolean] = Option(true),
-     hasImages:   Option[Boolean] = Option(true),
-     hasVideos:   Option[Boolean] = Option(false))
+case class BasicRule(keyword: String, emoji: String, containsUserId: String) extends Rule {
+  override def toPayload: PayloadEntry =
+    PayloadEntry(value = Group(keyword))
+      .flatMap(payload => PayloadEntry(And(emoji, payload.value)))
+      .flatMap(payload => PayloadEntry(And(AppendAt(containsUserId), payload.value)))
 
-/*
- Represents data required for filtered Stream Endpoint, only keyword is required, others are optional
- */
-final case class Rule (
-     keyword:         String,
-     userId:          Option[Long] = None,
-     emoji:           Option[String] = None,
-     phrase:          Option[String] = None,
-     hashtags:        Option[String] = None,
-     fromUser:        Option[String] = None,
-     toUser:          Option[String] = None,
-     url:             Option[String] = None,
-     retweetsOfUser:  Option[String] = None,
-     context:         Option[String] = None,
-     entity:          Option[String] = None,
-     conversationId:  Option[Long] = None,
-     lang:            Option[String] = Option("de"),
-     ruleOptions:     RuleOptions = RuleOptions()) {
+  def toBasicRule: PayloadEntry = toPayload
+}
 
-  def toBasicPayload: RulePayload = {
-    RulePayload(keyword)
-  }
+case class StandardRule(override val keyword: String,
+                   override val emoji: String,
+                   override val containsUserId: String, // including the @ character
+                   phrase: String,
+                   hashtags: String,
+                   url: String) extends BasicRule(keyword, emoji, containsUserId) {
 
-  def toPayload: RulePayload = {
-    ???
-  }
+  val isReply: Option[Boolean]=Option(false)
+  val hasHashtags: Option[Boolean]=Option(true)
+
+  override def toPayload: PayloadEntry =
+    toBasicRule
+      .flatMap(payload => PayloadEntry(And(Group(phrase), payload.value)))
+      .flatMap(payload => PayloadEntry(And(hashtags, payload.value)))
+      .flatMap(payload => PayloadEntry(And(url, payload.value)))
+
+  def toStandardRule: PayloadEntry = toPayload
+}
+
+case class AdvancedRule(override val keyword: String,
+                   override val emoji: String,
+                   override val containsUserId: String,
+                   override val phrase: String,
+                   override val hashtags: String,
+                   override val url: String,
+                   fromUser: String, // excluding the @ character or the user's numeric user ID
+                   toUser: String,  // excluding the @ character or the user's numeric user ID
+                   retweetsOfUser: String // excluding the @ character or the user's numeric user ID
+              ) extends StandardRule(keyword, emoji, containsUserId, phrase, hashtags, url) {
+
+  val hasLinks: Option[Boolean]=Option(true)
+  val hasMedia: Option[Boolean]=Option(true)
+  val hasImages: Option[Boolean]=Option(true)
+
+  override def toPayload: PayloadEntry =
+    toStandardRule
+      .flatMap(payload => PayloadEntry(And(Append("from:", fromUser), payload.value)))
+      .flatMap(payload => PayloadEntry(And(Append("to:", toUser), payload.value)))
+      .flatMap(payload => PayloadEntry(And(Append("retweetOf:", retweetsOfUser), payload.value)))
+
+  def toAdvancedRule: PayloadEntry = toPayload
+}
+
+case class FullRule(override val keyword: String,
+               override val emoji: String,
+               override val containsUserId: String,
+               override val phrase: String,
+               override val hashtags: String,
+               override val url: String,
+               override val fromUser: String,
+               override val toUser: String,
+               override val retweetsOfUser: String,
+               context: String,
+               entity: String,
+               conversationId: String // matches Tweets that share a common conversation ID
+            ) extends AdvancedRule(keyword, emoji, containsUserId, phrase, hashtags, url, fromUser, toUser, retweetsOfUser) {
+  val hasVideos: Option[Boolean]=Option(false)
+
+  override def toPayload: PayloadEntry =
+    toAdvancedRule
+      .flatMap(payload => PayloadEntry(And(Append("context:", context), payload.value)))
+      .flatMap(payload => PayloadEntry(And(Append("entity:", entity), payload.value)))
+      .flatMap(payload => PayloadEntry(And(Append("conversationId:", conversationId), payload.value)))
 }
 
 // provides Json Unmarshalling utility
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val ruleOptionsFormat: RootJsonFormat[RuleOptions] = jsonFormat8(RuleOptions)
-  implicit val ruleFormat: RootJsonFormat[Rule] = jsonFormat14(Rule)
+  implicit val basicRuleFormat: RootJsonFormat[BasicRule] = jsonFormat3(BasicRule)
+  implicit val standardRuleFormat: RootJsonFormat[StandardRule] = jsonFormat6(StandardRule)
+  implicit val advancedRuleFormat: RootJsonFormat[AdvancedRule] = jsonFormat9(AdvancedRule)
+  implicit val fullRuleFormat: RootJsonFormat[FullRule] = jsonFormat12(FullRule)
 }
