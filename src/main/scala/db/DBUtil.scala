458265b047f3ff0aaec958b1e406514fb03e956c
DY <Dong.Yang@Student.HTW-Berlin.de>
2021-07-02 16:40:26 +0200

    fix db settings

--- src/main/scala/db/DBUtil.scala
+++ src/main/scala/db/DBUtil.scala
@@ -4,11 +4,14 @@ import reactivemongo.api.bson.collection.BSONCollection
 
 import scala.concurrent.{ExecutionContext, Future}
 import reactivemongo.api.{AsyncDriver, DB, MongoConnection}
-import tweets.Tweet
 
-object DbWriter {
+trait DBUtil {
+  def GetCollection(collection: String): Future[BSONCollection]
+}
+
+object DBUtil extends DBUtil {
   // My settings (see available connection options)
-  val mongoUri = "mongodb://localhost:27017/dataZone?authMode=scram-sha1"
+  val mongoUri = "mongodb://localhost:27017/dataZone"
 
   // TODO: maybe application context
   import ExecutionContext.Implicits.global // use any appropriate context
@@ -20,16 +23,6 @@ object DbWriter {
   val futureConnection: Future[MongoConnection] = driver.connect(mongoUri)
   def dataZone: Future[DB] = futureConnection.flatMap(_.database("dataZone"))
 
-  def tweetsCollection: Future[BSONCollection] = dataZone.map(_.collection("tweets"))
-
-  // Write Documents: insert
-  // implicit def twitterWriter: BSONDocumentWriter[Tweet] = Macros.writer[Tweet]
-  // or provide a custom one
-
-  // use personWriter
-  def createTweet(tweet: Tweet): Future[Unit] =
-    tweetsCollection.flatMap(_.insert.one(tweet).map(_ => {}))
-
-  // implicit def tweetsReader: BSONDocumentReader[Tweet] = Macros.reader[Tweet]
-  // or provide a custom one
+  override def GetCollection(collection: String): Future[BSONCollection] =
+    dataZone.map(_.collection(collection))
 }
