3fe713de71f83e754624d2f542d9d4873077a0dd
DY <Dong.Yang@Student.HTW-Berlin.de>
2021-07-20 19:43:41 +0200

    fix tests

--- src/test/scala/test/CreateTweetsTest.scala
+++ src/test/scala/test/CreateTweetsTest.scala
@@ -5,15 +5,75 @@ import tweets.Tweet
 import utils.JSONParser
 
 class CreateTweetsTest extends AnyFunSuite{
+  val nonValidateJson: String =
+    """
+      |"nonValidateJson": "iaminvalidateJSONDATA"
+      |""".stripMargin
+
+  val emptyJson: String =
+    """
+      |{
+      |
+      |}
+      |""".stripMargin
+
   val basicDocument: String =
     """
       |{
       |  "data": {
-      |    "author_id": "25788488",
-      |    "created_at": "2021-07-12T09:49:54.000Z",
-      |    "id": "1414522394253877249",
+      |    "author_id": "722026255",
+      |    "conversation_id": "1416693550498996224",
+      |    "created_at": "2021-07-18T12:48:40.000Z",
+      |    "geo": {},
+      |    "id": "1416741710198034433",
+      |    "in_reply_to_user_id": "60372896",
+      |    "lang": "und",
+      |    "possibly_sensitive": false,
+      |    "source": "Twitter for Android",
+      |    "text": "@ID0N0I https://t.co/oA0mZOqk5b"
+      |  }
+      |}
+      |""".stripMargin
+
+  val publicMetrics: String =
+    """
+      |{
+      |  "data": {
+      |    "author_id": "971652661920505856",
+      |    "conversation_id": "1416741711758258181",
+      |    "created_at": "2021-07-18T12:48:40.000Z",
+      |    "geo": {},
+      |    "id": "1416741711758258181",
       |    "lang": "en",
-      |    "text": "RT @bysau_: tungsten cat's delivery service #ironmouseart @ironmouse https://t.co/FqDONJD4ls"
+      |    "possibly_sensitive": false,
+      |    "public_metrics": {
+      |      "retweet_count": 208,
+      |      "reply_count": 0,
+      |      "like_count": 0,
+      |      "quote_count": 0
+      |    },
+      |    "source": "Twitter for iPhone",
+      |    "text": "RT @twicebot_: 210627 twicetagram story update: https://t.co/huvNNz6Hfs\n\n#TWICE #트와이스 #Taste_of_Love https://t.co/x40vCFRrQA"
+      |  }
+      |}
+      |""".stripMargin
+
+  val nonPublicMetrics: String =
+    """
+      |{
+      |  "data": {
+      |    "author_id": "866976681554137088",
+      |    "conversation_id": "1416741712119025664",
+      |    "created_at": "2021-07-18T12:48:40.000Z",
+      |    "id": "1416741712119025664",
+      |    "lang": "th",
+      |    "non_public_metrics": {
+      |      "impression_count": 99,
+      |      "url_link_clicks": 37,
+      |      "user_profile_clicks": 22
+      |    },
+      |    "source": "Twitter for iPhone",
+      |    "text": "RT @Stray_cat_22: อยากกลับไปใช้ธงชาติแบบนี้ #ล้มราชวงศ์จักรี https://t.co/KiekIJlIbi"
       |  }
       |}
       |""".stripMargin
@@ -87,49 +147,28 @@ class CreateTweetsTest extends AnyFunSuite{
       |}
       |""".stripMargin
 
-  val entitiesWithUsers: String =
-    """
-      |{
-      |  "data": {
-      |    "author_id": "3038006302",
-      |    "created_at": "2021-07-12T09:50:11.000Z",
-      |    "entities": {
-      |      "mentions": [
-      |        {
-      |          "start": 3,
-      |          "end": 13,
-      |          "username": "k_tea_cat",
-      |          "id": "2536515190"
-      |        }
-      |      ]
-      |    },
-      |    "id": "1414522467939401733",
-      |    "lang": "en",
-      |    "text": "RT @k_tea_cat: Police be like: https://t.co/cOPazeSGZu"
-      |  }
-      |}
-      |""".stripMargin
-
   val entitiesWithUrls: String =
     """
       |{
       |  "data": {
-      |    "author_id": "1190100095024406528",
-      |    "created_at": "2021-07-12T09:50:05.000Z",
+      |    "author_id": "939723026",
+      |    "conversation_id": "1416741685808205825",
+      |    "created_at": "2021-07-18T12:48:34.000Z",
       |    "entities": {
       |      "urls": [
       |        {
-      |          "start": 45,
-      |          "end": 68,
-      |          "url": "https://t.co/bmDyBoyB2i",
-      |          "expanded_url": "https://twitter.com/billieloirinha/status/1414522442870136835/photo/1",
-      |          "display_url": "pic.twitter.com/bmDyBoyB2i"
+      |          "start": 78,
+      |          "end": 101,
+      |          "url": "https://t.co/3aEKtqReIm",
+      |          "expanded_url": "https://twitter.com/gnfhandholder/status/1416741685808205825/photo/1",
+      |          "display_url": "pic.twitter.com/3aEKtqReIm"
       |        }
       |      ]
       |    },
-      |    "id": "1414522442870136835",
-      |    "lang": "pt",
-      |    "text": "ela tá o próprio meme \"peituda metida\" sério https://t.co/bmDyBoyB2i"
+      |    "id": "1416741685808205825",
+      |    "lang": "en",
+      |    "source": "Twitter for iPhone",
+      |    "text": "i only have the dreamhangout version of this meme and it only makes it sadder https://t.co/3aEKtqReIm"
       |  }
       |}
       |""".stripMargin
@@ -236,6 +275,7 @@ class CreateTweetsTest extends AnyFunSuite{
       |  "matching_rules": []
       |}
       |""".stripMargin
+
   val withMatchingRules: String =
     """
       |{
@@ -409,80 +449,79 @@ class CreateTweetsTest extends AnyFunSuite{
       |}
       |""".stripMargin
 
+  test("non validate json data") {
+    val tweet = Tweet.createTweet(nonValidateJson)
+    assert(tweet.isEmpty)
+  }
+
+  test("empty json data") {
+    val tweet = Tweet.createTweet(emptyJson)
+    assert(tweet.isEmpty)
+  }
+
   test("build basic tweet") {
-    val jsonMap = JSONParser.parseJson(basicDocument).get
-    val tweet = Tweet.buildBasicTweet(jsonMap)
-    println(tweet.get)
+    val tweet = Tweet.createTweet(basicDocument)
+    println(tweet)
+  }
+
+  test("build tweet with public metrics") {
+    val tweet = Tweet.createTweet(publicMetrics)
+    println(tweet)
+  }
+
+  test("build tweet with non public metrics") {
+    val tweet = Tweet.createTweet(nonPublicMetrics)
+    println(tweet)
   }
 
   test("build tweet with empty context") {
-    val jsonMap = JSONParser.parseJson(emptyContext).get
-    val tweet = Tweet.buildBasicTweet(jsonMap)
-    val dataMap = jsonMap("data").asInstanceOf[Map[String, Any]]
-    val empty = Tweet.applyContext(tweet.get, dataMap)
-    println(empty)
+    val tweet = Tweet.createTweet(emptyContext)
+    println(tweet)
   }
 
   test("build tweet with context") {
-    val jsonMap = JSONParser.parseJson(withContext).get
-    val tweet = Tweet.buildBasicTweet(jsonMap)
-    val dataMap = jsonMap("data").asInstanceOf[Map[String, Any]]
+    val dataMap = JSONParser.parseJson(withContext).get("data").asInstanceOf[Map[String, Any]]
+    val tweet = Tweet.buildBasicTweet(dataMap)
     val context = Tweet.applyContext(tweet.get, dataMap)
     println(context)
   }
 
   test("build tweet empty entities") {
-    val jsonMap = JSONParser.parseJson(emptyEntities).get
-    val tweet = Tweet.buildBasicTweet(jsonMap)
-    val dataMap = jsonMap("data").asInstanceOf[Map[String, Any]]
+    val dataMap = JSONParser.parseJson(withContext).get("data").asInstanceOf[Map[String, Any]]
+    val tweet = Tweet.buildBasicTweet(dataMap)
     val empty = Tweet.applyEntities(tweet.get, dataMap)
     println(empty)
   }
 
-  test("build tweet with users within entities") {
-    val jsonMap = JSONParser.parseJson(entitiesWithUsers).get
-    val tweet = Tweet.buildBasicTweet(jsonMap)
-    val dataMap = jsonMap("data").asInstanceOf[Map[String, Any]]
-    val users = Tweet.applyEntities(tweet.get, dataMap)
-    println(users)
-  }
-
   test("build tweet with urls within entities") {
-    val jsonMap = JSONParser.parseJson(entitiesWithUrls).get
-    val tweet = Tweet.buildBasicTweet(jsonMap)
-    val dataMap = jsonMap("data").asInstanceOf[Map[String, Any]]
+    val dataMap = JSONParser.parseJson(entitiesWithUrls).get("data").asInstanceOf[Map[String, Any]]
+    val tweet = Tweet.buildBasicTweet(dataMap)
     val urls = Tweet.applyEntities(tweet.get, dataMap)
     println(urls)
   }
 
   test("build tweet with hashtags within entities") {
-    val jsonMap = JSONParser.parseJson(entitiesWithHashtags).get
-    val tweet = Tweet.buildBasicTweet(jsonMap)
-    val dataMap = jsonMap("data").asInstanceOf[Map[String, Any]]
+    val dataMap = JSONParser.parseJson(entitiesWithHashtags).get("data").asInstanceOf[Map[String, Any]]
+    val tweet = Tweet.buildBasicTweet(dataMap)
     val hashtags = Tweet.applyEntities(tweet.get, dataMap)
     println(hashtags)
   }
 
   test("build tweet with full entities") {
-    val jsonMap = JSONParser.parseJson(fullEntities).get
-    val tweet = Tweet.buildBasicTweet(jsonMap)
-    val dataMap = jsonMap("data").asInstanceOf[Map[String, Any]]
+    val dataMap = JSONParser.parseJson(fullEntities).get("data").asInstanceOf[Map[String, Any]]
+    val tweet = Tweet.buildBasicTweet(dataMap)
     val full = Tweet.applyEntities(tweet.get, dataMap)
     println(full)
   }
 
   test("build tweet with empty matching rules") {
-    val jsonMap = JSONParser.parseJson(emptyMatchingRules).get
-    val tweet = Tweet.buildBasicTweet(jsonMap)
-    val emptyRules = Tweet.applyMatchingRules(tweet.get, jsonMap)
-    println(emptyRules)
+    val tweet = Tweet.createTweet(emptyMatchingRules)
+    println(tweet)
   }
 
   test("build tweet with matching rules") {
-    val jsonMap = JSONParser.parseJson(withMatchingRules).get
-    val tweet = Tweet.buildBasicTweet(jsonMap)
-    val matchingRules = Tweet.applyMatchingRules(tweet.get, jsonMap)
-    println(matchingRules)
+    val tweet = Tweet.createTweet(withMatchingRules)
+    println(tweet)
   }
 
   test("create tweet") {
